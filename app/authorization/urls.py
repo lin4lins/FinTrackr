c8b1a409e1ed27442ea2caad4a8d420bbbcc0f7f
Yevhenii Diemientiev <yevhenii.diemientiev.pe@gmail.com>
2023-05-21 22:36:31 +0300

    Add Black check by GitHub actions. Update the code according to Black's suggestions.

--- app/authorization/urls.py
+++ app/authorization/urls.py
@@ -3,7 +3,7 @@ from .views import SignUpView
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
-    path('login/', LoginView.as_view(template_name='authorization/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path("signup/", SignUpView.as_view(), name="signup"),
+    path("login/", LoginView.as_view(template_name="authorization/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
