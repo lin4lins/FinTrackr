ea839c52ea21c0fa87de92f0a27722e57a9a0404
Yevhenii Diemientiev <yevhenii.diemientiev.pe@gmail.com>
2023-05-29 21:54:45 +0300

    Add diagrams and page navigation.

--- app/core/views/home.py
+++ app/core/views/home.py
@@ -7,7 +7,7 @@ from django.views import View
 class HomeView(LoginRequiredMixin, View):
     login_url = reverse_lazy("login")
     template_name = "core/home.html"
-    redirect_url = reverse_lazy("feed")
+    redirect_url = reverse_lazy("home")
 
     def get(self, request):
-        return render(request, self.template_name, {"nbar": "home"})
+        return render(request, self.template_name, {"active_page": "home"})
---
+++ app/js/home/cash_flow.js
@@ -0,0 +1,30 @@
+google.charts.load('current', {packages: ['corechart', 'bar']});
+google.charts.setOnLoadCallback(drawMultSeries);
+
+function drawMultSeries() {
+    var data = google.visualization.arrayToDataTable([
+       ['Type', 'Value', { role: 'style' }],
+       ['Income', 219.45, '#28CB7C' ],
+       ['Expense', 196.30, '#E74C3C'],
+    ]);
+
+    var view = new google.visualization.DataView(data);
+    view.setColumns([0, 1,
+                     { calc: "stringify",
+                       sourceColumn: 1,
+                       type: "string",
+                       role: "annotation" },
+                     2]);
+
+    var options = {
+      hAxis: {
+        title: '',
+        minValue: 0,
+      },
+      bar: {groupWidth: "95%"},
+      legend: { position: "none" },
+    };
+
+    var chart = new google.visualization.BarChart(document.getElementById('home_cash_flow'));
+    chart.draw(view, options);
+}
