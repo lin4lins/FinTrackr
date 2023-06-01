22c81b0ed591b9ca65de83a9bba360f79bf21273
Elina Shramko <elina.vl.shramko@gmail.com>
2023-06-01 14:22:36 +0300

    Create Category creation process

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from core.views.home import HomeView
+from core.views.category import CategoryCreateView
 
 urlpatterns = [
     path("home/", HomeView.as_view(), name="home"),
+    path("category/", CategoryCreateView.as_view(), name="category"),
 ]
