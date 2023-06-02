c372f1c6269da373180ea9621995dda7a55ce063
Elina Shramko <elina.vl.shramko@gmail.com>
2023-06-02 16:21:27 +0300

    Update category Creation and showing & Add delete for categories

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
-
+from core.views.category import CategoryDetailView, CategoryView
 from core.views.home import HomeView
-from core.views.category import CategoryCreateView
+from django.urls import path
 
 urlpatterns = [
     path("home/", HomeView.as_view(), name="home"),
-    path("category/", CategoryCreateView.as_view(), name="category"),
+    path("category/", CategoryView.as_view(), name="category"),
+    path("category/<int:category_id>/", CategoryDetailView.as_view(), name="category-detail"),
 ]
--- app/core/views/category.py
+++ app/core/views/category.py
@@ -1,20 +1,23 @@
+from core.forms import CategoryForm
+from core.models import Category
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db import IntegrityError
-from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
 from django.views import View
 
-from core.forms import CategoryForm
 
-
-class CategoryCreateView(LoginRequiredMixin, View):
+class CategoryView(LoginRequiredMixin, View):
     login_url = reverse_lazy("login")
-    template_name = "core/category-create.html"
+    template_name = "core/category.html"
     success_url = reverse_lazy("category")
 
     def get(self, request):
+        user = request.user
+        user_categories = Category.objects.filter(user=user).all()
         form = CategoryForm()
-        return render(request, self.template_name, {"form": form})
+        return render(request, self.template_name, {"categories": user_categories, "form": form})
 
     def post(self, request):
         form = CategoryForm(request.POST)
@@ -25,7 +28,21 @@ class CategoryCreateView(LoginRequiredMixin, View):
                 category.save()
                 return redirect(self.success_url)
 
-            except IntegrityError as exc:
-                form.add_error("name", exc)
+            except IntegrityError:
+                form.add_error("name", "Така категорія вже існує")
 
-        return render(request, self.template_name, {"form": form})
+        user_categories = Category.objects.filter(user=request.user).all()
+        return render(request, self.template_name, {"categories": user_categories, "form": form})
+
+
+class CategoryDetailView(LoginRequiredMixin, View):
+    login_url = reverse_lazy("login")
+    success_url = reverse_lazy("category")
+
+    def post(self):
+        pass
+
+    def delete(self, request, category_id):
+        category = get_object_or_404(Category, id=category_id, user=request.user)
+        category.delete()
+        return HttpResponse(status=204)
