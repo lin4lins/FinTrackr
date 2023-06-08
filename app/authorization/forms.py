0a5ac92df1e4b28bda25b31c1d14eb84adda3f5c
Elina Shramko <elina.vl.shramko@gmail.com>
2023-06-08 19:48:59 +0300

    Update login & signup templates

--- app/authorization/forms.py
+++ app/authorization/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 from .models import User
 
@@ -7,6 +7,19 @@ from .models import User
 class SignUpForm(UserCreationForm):
     email = forms.EmailField()
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["email"].label = "Електронна пошта"
+        self.fields["password1"].label = "Пароль"
+        self.fields["password2"].label = "Повторіть пароль"
+
     class Meta:
         model = User
         fields = ("email", "password1", "password2")
+
+
+class CustomAuthenticationForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["username"].label = "Електронна пошта"
+        self.fields["password"].label = "Пароль"
