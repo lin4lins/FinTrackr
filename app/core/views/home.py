be836addd02da55ab4ade6f4310f43fe655cd485
Yevhenii Diemientiev <yevhenii.diemientiev.pe@gmail.com>
2023-06-11 18:06:35 +0300

    Add using data from database for last transactions chart.

--- app/core/views/home.py
+++ app/core/views/home.py
@@ -6,7 +6,7 @@ from django.urls import reverse_lazy
 from django.views import View
 
 from core.helpers.home.money_structure import CategoryStructureGenerator
-from core.models import Account
+from core.models import Account, Transaction
 
 
 class HomeView(LoginRequiredMixin, View):
@@ -18,15 +18,21 @@ class HomeView(LoginRequiredMixin, View):
         user_accounts = Account.objects.filter(user=request.user)
         selected_account = user_accounts[0]
 
+        user_categories = request.user.categories.all()
         structure_generator = CategoryStructureGenerator(
-            categories=request.user.categories.all(), account=selected_account
+            categories=user_categories, account=selected_account
         )
+
+        last_transactions = Transaction.objects.filter(account=selected_account).order_by(
+            "-created_at"
+        )[:4]
 
         template_data = {
             "active_page": "home",
             "accounts": user_accounts,
             "expense_structure_data": json.dumps(structure_generator.expense_structure_data),
             "income_structure_data": json.dumps(structure_generator.income_structure_data),
+            "last_transactions": last_transactions,
         }
 
         return render(request, self.template_name, template_data)
